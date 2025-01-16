4cf7fcdf13a92e440c9bece5115374b755cfd00b
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-16 17:22:54 -0600

    feat: create document with controller

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { configDotenv } from 'dotenv';
 
 import connectDB from './backend/config/db';
 
+import { addFailed } from './backend/controllers/failed.controller';
+
 const app = Express();
 
 app.use(cors());
@@ -12,6 +14,8 @@ configDotenv();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+addFailed();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
